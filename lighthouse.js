87c08000971d64754f156e87d685eefba82c9da1
Santosh Grampurohit <san_v500@yahoo.com>
2020-03-06 14:25:03 +0530

    wait

--- lighthouse.js
+++ lighthouse.js
@@ -1,7 +1,7 @@
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
 const fs = require('fs');
-
+let EXITCONDITION = false;
 function launchChromeAndRunLighthouse(url, opts, config = null) {
   return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
     opts.port = chrome.port;
@@ -19,16 +19,27 @@ const opts = {
 const validMetrics = {
   'first-contentful-paint': 0.65
 }
+
 // Usage:
 launchChromeAndRunLighthouse('https://techdoma.in', opts).then(results => {
   // Use results!
+  let ret = 0;
   const keys = Object.keys(validMetrics);
   keys.forEach(key => {
     const audit = results.audits[key];
     if(validMetrics[key] <= audit.score){
-      return -1;
+      ret = -1;
     }
   });
-  return 0;
+  EXITCONDITION = true;
+  console.log(ret);
+  return ret;
+  
   // fs.writeFileSync('result', JSON.stringify(results))
 });
+
+function wait () {
+  if (!EXITCONDITION)
+       setTimeout(wait, 1000);
+};
+wait();
